7e83e1e57715223bf1d7b6a3d7a750e6e6d1f424
zaid-ansar <145926509+zaid-ansar@users.noreply.github.com>
2023-09-27 18:00:32 +0000

    variable done

--- basics/variable.js
+++ basics/variable.js
@@ -1,1 +1,47 @@
-console.log("zaid");
+// console.log("zaid");
+// const accountId = 12345
+// let accountEmail = "zaid@google.com"
+// var accountPassword = "32489"
+// // accountCity = "jaipur"
+
+// // accountId = 2 //not allowd
+
+// accountId = "1223435"
+// accountEmail = "ansari@google.com"
+// accountPassword = "435334"
+// accountCity = "bihar"
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+// console.log(accountEmail);
+
+let score = "true"
+// console.log(typeof score);
+
+// ********* operations **************
+
+let value = 4
+let negvalue = -value;
+//console.log(negvalue);
+
+let str1 = "zaid"
+let str2 = " ansari"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// comparision
+
+//console.log(2>1);
+//console.log("2" === 2); // check
+
+// data types//
+// primitive data types
+// 7 types
+// string, number, boolean, null, undefined, symbol, Bigint
+
+
+// non primitive (reference)
+// array, object, functions
+
+
+
+
